3af2365e66fa86cbe5939f9d72cde1035b9cb086
ohdam <erfrr@naver.com>
2024-07-09 17:42:31 +0900

    [Add] add mutex review code
    - mutex 라이브러리 복습
    - lock_guard 개념 복습

---
+++ C++/mutex_ex.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+
+
+std::mutex mlock;
+std::vector<int32_t> v;
+
+// RAII Pattern을 통한 Lock 관리방법
+// std::lock_guard<std::mutex> lg(mlock);
+// std::unique_lock<std::mutex> ul(mlock, std::defer_lock);  std::defer_lock  \
+															--> lock에 관한 option에 해당한다.
+
+
+
+void Push(int32_t num)
+{
+	while (true)
+	{
+		mlock.lock();
+		v.push_back(num);
+		std::cout << "Push" << num << "\n";
+		mlock.unlock();
+	}
+}
+
+void Pop()
+{
+	while (true)
+	{
+		mlock.lock();
+		if(v.size() > 0) v.pop_back();
+		std::cout << "Pop\n";
+		mlock.unlock();
+	}
+}
+
+int main()
+{
+	std::thread t1(Push,1);
+	std::thread t2(Pop);
+
+	t1.join();
+	t2.join();
+}
