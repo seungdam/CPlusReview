ce3e43852327001c6340f17f0b49f0812d6dfef6
ohdam <erfrr@naver.com>
2024-07-30 23:34:27 +0900

    [Add] add RAII review code
    - RAII 관련 개념 주석 작성
    - 대표적인 RAII 객체인 lock_guard를 활용하는 코드 작성

--- ConcurrentProgramming/raii.cpp
+++ ConcurrentProgramming/raii.cpp
@@ -8,13 +8,48 @@
 //  ㄴ lock을 사용한 후 unlock을 수행하지 않은 경우
 
 
-// RAII (Resource Acquisition Is Initialization)
+//  RAII (Resource Acquisition Is Initialization)
 // 위와 같은 상황을 방지하기 위해, 우리는 lock_guard, unique_lock, shared_lock
 // 과 같이 뮤텍스를 RAII에 의거해 처리하는 함수를 반드시 사용하는 것이 좋다.
 // 단순하게 사용하는 경우 lock_guard를..
 // 고급스러운 기능을 사용하는 경우 unique_lock, shared_lock을 사용한다.
 
+using namespace std;
+
+mutex m;
+
+class WorkerObject
+{
+	static int32 cnt;
+public:
+#pragma region lock_guard
+	void operator()()
+	{
+
+		// exclusive-lock
+		lock_guard lg(m);
+		std::cout << "\033[32m[" << this_thread::get_id() << "]\033[0m" << "\033[31m Get Exclusive Locked.\033[0m\n";
+		++cnt;
+		std::cout << "	Current Conunt: " << "\033[33m" << cnt << "\033[0m\n";
+	}
+#pragma endregion
+};
+
+
+
+int32 WorkerObject::cnt = 0;
+
 int main()
 {
+	thread t[100];
 
+	for (auto& i : t)
+	{
+		i = thread(WorkerObject());
+	}
+
+	for (auto& i : t)
+	{
+		i.join();
+	}
 }
