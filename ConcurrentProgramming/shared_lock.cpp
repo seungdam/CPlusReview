d82d8ff5f1c78b5bcb8957a3125a1569fec8c078
ohdam <erfrr@naver.com>
2024-07-30 23:36:21 +0900

    [Add] add Reader-Writer Lock review code
    - shared_mutex(shared_timed_mutex)와 RAII 객체를 통해 reader-writer-lock 실습 코드 작성

--- ConcurrentProgramming/shared_lock.cpp
+++ ConcurrentProgramming/shared_lock.cpp
@@ -1,8 +1,72 @@
 #include "pch.h"
 #include <shared_mutex>
+#include <random>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+// *** shared_lock
+// reader-writer lock¿ª ¿ß«— RAII Wrapper class
+
+random_device rd;
+default_random_engine dre(rd());
+uniform_int_distribution uid(1, 10);
+
+shared_timed_mutex stm;
+shared_mutex sm;
+
+
+string s = "Count Value:  ";
+
+void Writer()
+{
+	static int32 cnt = 0;
+	char ccnt[2];
+	
+	stringstream ss;
+	ss << std::this_thread::get_id();
+	int32 id = std::stoull(ss.str());
+
+	while (cnt < 10)
+	{
+		unique_lock<shared_timed_mutex> ul(stm);
+		printf("[\033[32m%5d|\033[33mWriter\033[0m]\033[31m Now Updating...\033[0m\n", id);
+		::_itoa_s(cnt++, ccnt, _countof(ccnt), 10);
+		s.pop_back();
+		s.append(ccnt);
+		this_thread::sleep_for(2s);
+		printf("[\033[32m%5d|\033[33mWriter\033[0m] Updating Ended...\n", id);
+	}
+
+	printf("[\033[32m%5d|\033[33mWriter\033[0m] Job Finished\n", id);
+}
+
+void Reader()
+{
+	stringstream ss;
+	ss << std::this_thread::get_id();
+	int32 id = std::stoull(ss.str());
+
+	this_thread::sleep_for(std::chrono::seconds(uid(dre)));
+	shared_lock<shared_timed_mutex> ul(stm);
+	printf("	[\033[32m%5d|\033[34mReader\033[0m]\033[36m %s\033[0m\n", id, s.c_str());
+}
 
 
 int main()
 {
+	thread w(Writer);
+	thread r[100];
 
+	for (auto& i : r)
+	{
+		i = thread(Reader);
+	}
+
+	w.join();
+
+	for (auto& i : r)
+	{
+		i.join();
+	}
 }
---
+++ ConcurrentProgramming/shared_lock2.cpp
@@ -0,0 +1,87 @@
+#include "pch.h"
+#include <shared_mutex>
+#include <unordered_map>
+#include <sstream>
+#include <random>
+
+using namespace std;
+
+random_device rd;
+default_random_engine dre(rd());
+uniform_int_distribution uid(0, 30);
+uniform_int_distribution uid2(1900, 1999);
+
+shared_timed_mutex stm;
+shared_mutex sm;
+
+unordered_map<string, int32> dataset{ {"OHSD",2000}, {"KIMSN",1999}, {"PARKBS",1997} ,{"KIMSY",1934} };
+
+string s[5]{ "OHSD","KIMSN","PARKBS","KIMSY","KANGHS"};
+
+void Writer(string& key)
+{
+	stringstream ss;
+	ss << std::this_thread::get_id();
+	int32 id = std::stoull(ss.str());
+
+
+	unique_lock<shared_timed_mutex> ul(stm);
+	printf("[\033[32m%5d|\033[33mWriter\033[0m]\033[1;5;31m Now Updating...\033[0m\n", id);
+	dataset[key] = uid2(dre);
+	this_thread::sleep_for(3s);
+
+	printf("[\033[32m%5d|\033[33mWriter\033[0m] Update Ended...\n", id);
+	
+	printf("\n	[\033[105m=== New Data Set ===\033[0m\n");
+	for (auto& i : dataset)
+	{
+		printf("	   [\033[103;34m%6s|\033[0;36m%d\033[0m] \n", i.first.c_str(), i.second);
+	}
+}
+
+void Reader(const string& key)
+{
+	stringstream ss;
+	ss << std::this_thread::get_id();
+	int32 id = std::stoull(ss.str());
+
+	this_thread::sleep_for(std::chrono::seconds(uid(dre)));
+	shared_lock<shared_timed_mutex> ul(stm);
+	
+	auto item = dataset.find(key);
+	if (item != dataset.end())
+	{
+		printf("[\033[1;32m%5d|\033[1;36mReader\033[0m] Data Founded: [\033[103;34m%6s\033[0m]\033[36m %d\033[0m\n", id, item->first.c_str(), item->second);
+	}
+	else
+	{
+		printf("	\033[1;31m Data Not Found:\033[0m %6s\n", key.c_str());
+	}
+}
+
+int main()
+{
+	thread w[20];
+	thread r[30];
+
+	for (auto& i : w)
+	{
+		i = thread(Writer, ref(s[uid(dre) % 4]));
+	}
+
+	for (auto& i : r)
+	{
+		i = thread(Reader, ref(s[uid(dre) % 4]));
+	}
+
+
+	for (auto& i : w)
+	{
+		i.join();
+	}
+
+	for (auto& i : r)
+	{
+		i.join();
+	}
+}
