d1feb2370acc05e8ccd33704686d9fcc672113a1
ohdam <erfrr@naver.com>
2024-07-31 22:44:44 +0900

    [Modify] make macro about ansii escape code for printf

--- ConcurrentProgramming/pch.h
+++ ConcurrentProgramming/pch.h
@@ -17,3 +17,14 @@ using uint8  = unsigned __int32;
 using uint16 = unsigned __int32;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int32;
+
+#define t_set "\033["
+#define t_reset "\033[0m"
+#define t_bold "1;"
+
+#define t_color_r "31m"
+#define t_color_g "32m"
+#define t_color_y "33m"
+#define t_color_b "34m"
+#define t_color_p "35m"
+#define t_color_c "36m"
